b2e95ef39c13f461add02b39c1614448679e1505
Jake Hutson <100004059+JakeHutson42@users.noreply.github.com>
2026-06-25 17:09:08 +0100

    Add files via upload

---
+++ PersonalTools/Entities/MediaExtractor/MediaItemObj.cs
@@ -0,0 +1,28 @@
+﻿namespace PersonalTools.Entities.MediaExtractor
+{
+    public class MediaItemObj
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        public string Name { get; set; } = "";
+        public string Url { get; set; } = "";
+
+        public string Type { get; set; } = "";
+        public string Extension { get; set; } = "";
+
+        public long SizeBytes { get; set; }
+        public string SizeFormatted { get; set; } = "";
+
+        // NEW
+        public bool Selected { get; set; }
+
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public string Dimensions =>
+            Width > 0 && Height > 0 ? $"{Width} x {Height}" : "";
+
+        public double DurationSeconds { get; set; }
+        public string DurationFormatted { get; set; } = "";
+    }
+}
